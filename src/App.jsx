d3bfb060b644b54ae6cb7ad4dbd916fd3d999f9c
MarionbDev <marionbaston84@gmail.com>
2024-04-11 18:14:06 +0200

    sauvegarde exo1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const App = () => {
-	// Write the code of your component here
+  const [showArticle, setShowArticle] = useState(false);
+
+  const handleClick = () => {
+    setShowArticle(!showArticle);
+  };
+
+  return (
+    <>
+      <h1>Mini Blog</h1>
+      <button
+        className={`btn btn--primary ${showArticle ? "btn--danger" : ""} `}
+        onClick={handleClick}
+      >
+        {showArticle ? "Hide Article" : "Show Article"}
+      </button>
+      {showArticle && (
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quam
+          doloremque atque aliquid. Adipisci dolor nisi neque quae recusandae
+          harum non architecto accusamus dolore doloremque. Consequuntur rerum
+          mollitia quidem id!
+        </p>
+      )}
+    </>
+  );
 };
 
 export default App;
